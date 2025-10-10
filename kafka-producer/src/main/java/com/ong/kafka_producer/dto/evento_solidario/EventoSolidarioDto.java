709f5d31a8eb6aead2a082c3e0b37f35381c524f
Mariano Di gangi <marianodigangi1@gmail.com>
2025-10-10 16:31:21 -0300

    Agrego Dto evento solidario

--- kafka-producer/src/main/java/com/ong/kafka_producer/dto/evento_solidario/EventoSolidarioDto.java
+++ kafka-producer/src/main/java/com/ong/kafka_producer/dto/evento_solidario/EventoSolidarioDto.java
@@ -1,2 +1,18 @@
-package com.ong.kafka_producer.dto.evento_solidario;public class EventoSolidarioDto {
+package com.ong.kafka_producer.dto.evento_solidario;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class EventoSolidarioDto {
+    private Long idOrganizacion;
+    private Long idEvento;
+    private String nombreEvento;
+    private String descripcion;
+    private LocalDateTime fechaHoraEvento;
 }
